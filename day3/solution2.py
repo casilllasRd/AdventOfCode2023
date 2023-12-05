d8e2abea85578d64654668961e3630964f8305f7
casilllasRd <91971186+casilllasRd@users.noreply.github.com>
2023-12-05 01:27:30 -0500

    Add files via upload

--- day3/solution2.py
+++ day3/solution2.py
@@ -1,55 +1,62 @@
-import math
+def solution():
+    puzzle_input = open("day3/puzzle-input.txt")
+    puzzle_data = puzzle_input.read()
+    engine_data = puzzle_data.split("\n")
+
+    get_gear_ratio_sum(engine_data)
 
 
-def solution() -> None: 
-    puzzle_data: list[str] = get_puzzle_data()
-    answer: int = get_power_sum(puzzle_data)
+def get_gear_ratio_sum(data):
+    gear_locations = []
 
-    print(answer)
+    current_digits = []
+    locations = []
+    engine_numbers = []
+
+    for row, line in enumerate(data):
+        for col, char in enumerate(line):
+            if char == "*":
+                gear_locations.append([
+                    (col - 1, row - 1), (col + 0, row - 1), (col + 1, row - 1),
+                    (col - 1, row + 0), (col + 0, row + 0), (col + 1, row + 0),
+                    (col - 1, row + 1), (col + 0, row + 1), (col + 1, row + 1),    
+                ])
+
+            if char.isdigit():
+                current_digits.append(char)
+                locations.append((col, row))
+
+                if col == len(data[row]) - 1:
+                    engine_numbers.append({
+                        "num": "".join(current_digits),
+                        "locations": locations,
+                        "gear adjacent": False
+                    })
+                    current_digits = []
+                    locations = []
+
+                elif not data[row][col + 1].isdigit():
+                    engine_numbers.append({
+                        "num": "".join(current_digits),
+                        "locations": locations,
+                        "gear adjacent": False
+                    })
+                    current_digits = []
+                    locations = []
+
+    ratio_sum = 0
+    for gear in gear_locations:
+        adjacent_numbers = []
+        for engine_number in engine_numbers:
+            adjacent_cells = [l for l in engine_number["locations"] if l in gear]
+            if adjacent_cells:
+                adjacent_numbers.append(int(engine_number["num"]))
+        
+        print(adjacent_numbers)
+        if len(adjacent_numbers) == 2:
+            ratio_sum += (adjacent_numbers[0] * adjacent_numbers[1])
+
+    print(ratio_sum)
 
 
-def get_puzzle_data() -> list[str]:
-    file: str = "puzzle-input.txt"
-    puzzle_input = open(file)
-    puzzle_data: list[str] = puzzle_input.readlines()
-
-    return puzzle_data
-
-
-def get_power_sum(puzzle_data: list[str]) -> int:
-    powers_sum: int = 0
-
-    for game in puzzle_data:
-        game_samples: dict[int] = {
-            "red": 0,
-            "green": 0,
-            "blue": 0
-        }
-        reveals: list[str] = get_reveals(game)
-
-        for reveal in reveals:
-            for color, current_sample in game_samples.items():
-                if color in reveal:
-                    cube_num: int = get_digits_from_string(reveal)
-                    game_samples[color] = max(cube_num, current_sample)
-
-        powers_sum += get_power(game_samples)
-    
-    return powers_sum
-
-
-def get_reveals(game: str) -> list[str]:
-    reveals: str = game.split(":")[1]
-    return reveals.replace(";", ",").split(",")
-
-
-def get_digits_from_string(s: str) -> int:
-    digits: str = "".join([char for char in s if char.isdigit()])
-    return int(digits)
-
-
-def get_power(game_samples: dict[int]) -> int:
-    return math.prod(game_samples.values())
-
-
-solution()
+solution();
